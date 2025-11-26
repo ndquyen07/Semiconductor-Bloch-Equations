4794d795202bc83b20c61017cebb28c4e6ca1dcc
kakarot07 <ndquyen07@gmail.com>
2025-11-26 11:11:07 +0700

    update

--- plot.gnu
+++ plot.gnu
@@ -27,14 +27,16 @@ set key top right
 plot 'data/absorption_comparison_with.dat' using 1:2 with lines lw 2 lc rgb "blue" title 'With Coulomb', \
      'data/absorption_comparison_without.dat' using 1:2 with lines lw 2 lc rgb "red" title 'Without Coulomb'
 
-# Plot 4: Occupation f_n for selected levels (n=1, 5, 10)
+# Plot 4: Occupation f_n for selected levels (n=1, 2, 3, 4, 5)
 set xlabel "Time"
 set ylabel "Occupation f_n"
 set grid
 set key top left
 plot 'data/fn.dat' using 1:2 with lines lw 2 title 'n=1', \
-     'data/fn.dat' using 1:6 with lines lw 2 title 'n=5', \
-     'data/fn.dat' using 1:11 with lines lw 2 title 'n=10'
+     'data/fn.dat' using 1:3 with lines lw 2 title 'n=2', \
+     'data/fn.dat' using 1:4 with lines lw 2 title 'n=3', \
+     'data/fn.dat' using 1:5 with lines lw 2 title 'n=4', \
+     'data/fn.dat' using 1:6 with lines lw 2 title 'n=5'
 
 unset multiplot
 
--- src/utils.py
+++ src/utils.py
@@ -1,13 +1,11 @@
 import numpy as np
 
 def save_results(solver):
-        np.savetxt('data/fn.dat', np.column_stack((solver.t[:, np.newaxis], solver.f_n)), delimiter='\t')
-        np.savetxt('data/pn.dat', np.column_stack((solver.t[:, np.newaxis], solver.p_n.real, solver.p_n.imag)), delimiter='\t')
-        np.savetxt('data/population.dat', np.column_stack((solver.t, solver.population)), fmt="%.2f", delimiter='\t')
-        np.savetxt('data/polarization.dat', np.column_stack((solver.t, solver.polarization.real, solver.polarization.imag)), delimiter='\t')
-        np.savetxt('data/absorption_spectrum.dat', np.column_stack((solver.spectrum_energy, solver.absorption_spectrum)), delimiter='\t')
-
-
+        np.savetxt(f'data/fn_chi{solver.chi_0}.dat', np.column_stack((solver.t[:, np.newaxis], solver.f_n)), delimiter='\t')
+        np.savetxt(f'data/pn_chi{solver.chi_0}.dat', np.column_stack((solver.t[:, np.newaxis], solver.p_n.real, solver.p_n.imag)), delimiter='\t')
+        np.savetxt(f'data/population_chi{solver.chi_0}.dat', np.column_stack((solver.t, solver.population)), fmt="%.2f", delimiter='\t')
+        np.savetxt(f'data/polarization_chi{solver.chi_0}.dat', np.column_stack((solver.t, solver.polarization.real, solver.polarization.imag)), delimiter='\t')
+        np.savetxt(f'data/absorption_spectrum_chi{solver.chi_0}.dat', np.column_stack((solver.spectrum_energy, solver.absorption_spectrum)), delimiter='\t')
 def save_absorption_comparison(solver1, solver2):
-    np.savetxt('data/absorption_comparison_with.dat', np.column_stack((solver1.spectrum_energy, solver1.absorption_spectrum)), delimiter='\t')
-    np.savetxt('data/absorption_comparison_without.dat', np.column_stack((solver2.spectrum_energy, solver2.absorption_spectrum)), delimiter='\t')
+    np.savetxt(f'data/absorption_comparison_with_chi{solver1.chi_0}.dat', np.column_stack((solver1.spectrum_energy, solver1.absorption_spectrum)), delimiter='\t')
+    np.savetxt(f'data/absorption_comparison_without_chi{solver2.chi_0}.dat', np.column_stack((solver2.spectrum_energy, solver2.absorption_spectrum)), delimiter='\t')
